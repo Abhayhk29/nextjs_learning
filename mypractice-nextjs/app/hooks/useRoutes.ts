637e39d25d491a84b265f796a5d18c2a0a993437
abhayhk29 <abhayhk29@gmail.com>
2023-08-29 09:38:33 +0530

    initial changes

--- mypractice-nextjs/app/hooks/useRoutes.ts
+++ mypractice-nextjs/app/hooks/useRoutes.ts
@@ -7,7 +7,31 @@ import {signOut } from "next-auth/react";
 import useConversation from "./useConversation";
 
 const useRoutes = () => {
+    const pathname = usePathname();
+    const { conversionId }  = useConversation();
 
+    const routes = useMemo(() => [
+        {
+            label : 'Chat',
+            href: '/conversations',
+            icon: HiChat,
+            active : pathname === '/conversations' || !!conversionId
+        },
+        {
+            label : 'Users',
+            href: '/users',
+            icon: HiUser,
+            active : pathname === '/users'
+        },
+        {
+            label : 'Logout',
+            href: '#',
+            onClick: () => signOut(),
+            icon: HiArrowLeftOnRectangle
+        }
+    ],[pathname, conversionId])
+
+    return routes;
 }
 
 export default useRoutes
